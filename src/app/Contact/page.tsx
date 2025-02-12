07fca502914d02389d29b165144948fafa016f2b
shazia mahar <shaziamahar478@gmail.com>
2025-02-12 13:33:20 +0500

    rechecking

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -8,11 +8,11 @@ const Contact = () => {
     email: "",
     message: "",
   });
-  const handleChange = (e) => {
+  const handleChange = (e: { target: { name: any; value: any; }; }) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     console.log("Form Data Submitted:", formData);
   };
